a72206e0edd9b930cd59042fda87569cb148a262
Slavomir Vojacek <slavomir@adbrain.com>
2014-12-02 19:02:01 +0000

    added install.sh, updated bash profile settings

---
+++ bash/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+### @credits to https://github.com/samthebest
+
+# Install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew cask (makes it easier to install mac apps)
+brew install caskroom/cask/brew-cask
+
+# Install spectacle
+brew cask install spectacle
+
+# Install google-chrome
+brew cask install google-chrome
+
+# Install sublime-text
+brew cask install sublime-text
+
+# Install java
+brew cask install java
+
+# Install scala
+brew install scala
+
+# Install sbt
+brew install sbt
+
+# Install intellij-idea-ce (Community edition)
+brew cask install intellij-idea-ce
+
+# Install phpstorm
+brew cask install phpstorm
--- bash/profile.sh
+++ bash/profile.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+### Change prompt
 export PS1='[\t] \w/ '
 
-### Increase terminal history
+### Change editor
+export EDITOR=nano
+
+### Increase bash history to a million commands (should be enough)
 HISTFILESIZE=10000000
 
-### Alias for push
+### Add Alias for push.sh (can be found in the same repo dir)
 alias push='bash push.sh'
