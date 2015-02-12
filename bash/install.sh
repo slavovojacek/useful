5d9f690f69bc5ac8032e817a66978f195f06219e
Slavomir Vojacek <slavomir@adbrain.com>
2015-02-12 16:08:50 +0000

    added new commands to install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -5,11 +5,16 @@
 # Install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Before brew install, run brew doctor. If there is a problem with ownership
+# Before brew install, run brew doctor. If there is a problem with ownership,
 # take it using $ sudo chown -R $USER /usr/local/{DIR}
+#
+# Make sure to run brew update and brew upgrade periodically
 
 # Install Git
 brew install git
+
+# Install node
+brew install node
 
 # Install brew cask (makes it easier to install mac apps)
 brew install caskroom/cask/brew-cask
@@ -23,8 +28,14 @@ brew cask install google-chrome
 # Install sublime-text
 brew cask install sublime-text
 
-# Install node
-brew install node
+# Install intellij-idea-ce (Community edition)
+brew cask install intellij-idea-ce
+
+# Install phpstorm
+brew cask install phpstorm
+
+# Install ifstat
+brew cask install ifstat
 
 # Install java
 brew cask install java
@@ -35,11 +46,5 @@ brew install scala
 # Install sbt
 brew install sbt
 
-# Install intellij-idea-ce (Community edition)
-brew cask install intellij-idea-ce
-
-# Install phpstorm
-brew cask install phpstorm
-
-# Install ifstat
-brew cask install ifstat
+# Install imagemagick
+brew install imagemagick
